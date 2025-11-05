8318b40717dcd03e0084ff2a29386b8bbccc5027
Miguel Motta <93295363+MIGUEL-MOTTA-U@users.noreply.github.com>
2025-11-04 20:56:46 -0500

    feat: delete two photos

--- src/home/components/PhotoCarousel.tsx
+++ src/home/components/PhotoCarousel.tsx
@@ -3,14 +3,12 @@ import "./PhotoCarousel.css";
 
 import img1 from "../../assets/images/carousel/img1.jpg";
 import img2 from "../../assets/images/carousel/img2.jpg";
-import img3 from "../../assets/images/carousel/img3.jpg";
 import img5 from "../../assets/images/carousel/img5.jpg";
 import img6 from "../../assets/images/carousel/img6.jpg";
 import img8 from "../../assets/images/carousel/img8.jpg";
 import img9 from "../../assets/images/carousel/img9.jpg";
-import r7 from "../../assets/images/carousel/r7.jpg";
 
-const images = [img1, img2, img3, img9, img5, img6, r7, img8];
+const images = [img1, img2,  img9, img5, img6, img8];
 
 const getIndex = (idx: number) => (idx + images.length) % images.length;
 
